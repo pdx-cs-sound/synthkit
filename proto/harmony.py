8123bf7dc06cb6c8d37ce532a1f207ad19006b3b
Bart Massey <bart@cs.pdx.edu>
2019-06-01 01:20:50 -0700

    added autocorr-based wrapping; works but does not help

--- proto/harmony.py
+++ proto/harmony.py
@@ -13,17 +13,44 @@ import wavio
 samples = wavio.readwav("../loop.wav")
 nsamples = len(samples)
 
+# Maximum search for autocorrelator.
+ac_msecs = 200
+ac_samples = ac_msecs * 48000 // 1000
+
+# Sample length for autocorrelator.
+ac_length = ac_samples // 8
+
+# Do an autocorrelation to try to find a good place to
+# end the samples so they loop.
+cmax = None
+umax = None
+for t in range(ac_samples):
+    u = nsamples - ac_samples - ac_length + t
+    st = samples[:ac_length]
+    su = samples[u:u+ac_length]
+    corr = np.dot(st, su)
+    if cmax == None or corr > cmax:
+        cmax = corr
+        umax = u
+samples = samples[:umax + ac_length]
+nsamples = len(samples)
+
 # Milliseconds of lap window at beginning and end of harmony.
-lap_msecs = 20
+lap_msecs = 100
 lap_samples = lap_msecs * 48000 // 1000
 
 # Lap the samples.
 for i in range(lap_samples):
-    c = i / (lap_samples + 1)
+    c = i / (lap_samples - 1)
     samples[i] *= 1 - c
     samples[i] += c * samples[nsamples + i - lap_samples - 1]
 
-wavio.writewav("w.wav", np.array(samples))
+# Replicate the harmony for 5 seconds.
+nreplica = 5 * 48000
+replica = np.array([0]*nreplica, dtype=np.float)
+for i in range(nreplica):
+    replica[i] = samples[i % nsamples]
+wavio.writewav("ac.wav", replica)
 
 # Use an interpolation window this size around each sample.
 # Window should be odd.
