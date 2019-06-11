0d2f6d2af8a28ba18cc7d3e08fcb00814939f830
Bart Massey <bart@cs.pdx.edu>
2019-06-11 01:48:18 -0700

    attempted a port to cpal; failed
    Sadly, event_loop.run() wants a Send closure, while my
    Iterator trait object is not Send. IDK. Giving up for now.

--- src/play.rs
+++ src/play.rs
@@ -5,56 +5,61 @@
 
 //! Synthesizer audio player.
 
-use portaudio as pa;
-
 use std::error::Error;
+use std::io::{self, ErrorKind};
 
 use crate::*;
 
 /// Number of samples for a blocking write.
-/// XXX This has been carefully tuned to work
-/// around a `portaudio` bug: I do not suggest
-/// changing it.
 const OUT_FRAMES: usize = 12;
 
 /// Gather samples and post for playback.
 pub fn play(mut samples: Stream) -> Result<(), Box<Error>> {
 
-    // Callback supplies portaudio with a requested chunk of samples.
-
     // Create and initialize audio output.
-    let out = pa::PortAudio::new()?;
-    let mut settings = out.default_output_stream_settings(
-        1, // 1 channel.
-        SAMPLE_RATE as f64,
-        0_u32, // Least possible buffer.
-    )?;
-    settings.flags = pa::stream_flags::CLIP_OFF;
-    let mut stream = out.open_blocking_stream(settings)?;
+    let event_loop = cpal::EventLoop::new();
+    let device = cpal::default_output_device()
+        .ok_or_else(||
+            Box::new(io::Error::from(ErrorKind::ConnectionRefused)))?;
+    let target_rate = cpal::SampleRate(SAMPLE_RATE as u32);
+    let has_format = device
+        .supported_output_formats()?
+        .find(|&f| {
+            f.channels == 1 &&
+                f.min_sample_rate <= target_rate &&
+                f.max_sample_rate >= target_rate &&
+                f.data_type == cpal::SampleFormat::I16
+        })
+        .is_some();
+    if !has_format {
+        return Err(Box::new(cpal::FormatsEnumerationError::InvalidArgument));
+    }
+    let format = cpal::Format {
+        channels: 1,
+        sample_rate: target_rate,
+        data_type: cpal::SampleFormat::I16,
+    };
+    let stream = event_loop.build_output_stream(&device, &format)?;
+    event_loop.play_stream(stream);
 
-    stream.start()?;
-
-    let mut playing = true;
-    while playing {
-        stream.write(OUT_FRAMES as u32, |out| {
+    let samples = &mut std::sync::Arc::new(std::sync::Mutex::new(samples));
+    event_loop.run(|_stream, data| {
+        use cpal::UnknownTypeOutputBuffer::I16 as UTOB;
+        use cpal::StreamData::Output as SDO;
+        if let SDO { buffer: UTOB(mut out) } = data {
             let nout = out.len();
             for i in 0..nout {
-                match samples.next() {
+                match samples.lock().unwrap().next() {
                     Some(s) => out[i] = f32::floor(s * 32768.0f32) as i16,
-                    None => {
-                        for j in i..nout {
-                            out[j] = 0;
-                        }
-                        playing = false;
-                        return;
-                    },
+                    None => std::process::exit(0),
                 }
             }
-        })?;
-    }
+        } else {
+            panic!("unexpected output buffer type");
+        }
+    });
 
-    stream.stop()?;
-    stream.close()?;
-
-    Ok(())
+    // Should never actually be able to get here.
+    #[allow(unused)]
+    Err(Box::new(io::Error::from(ErrorKind::Interrupted)))
 }
