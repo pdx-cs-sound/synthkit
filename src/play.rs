8205ec3382814d994558bbd3cf6ddfaa13942653
Bart Massey <bart.massey@gmail.com>
2020-02-27 11:34:57 -0800

    started to update cpal to latest API

--- src/play.rs
+++ src/play.rs
@@ -9,6 +9,9 @@ use std::error::Error;
 use std::io::{self, ErrorKind};
 use std::sync::{Arc, Mutex};
 
+use cpal::*;
+use cpal::traits::*;
+
 use crate::*;
 
 use lazy_static::*;
@@ -31,12 +34,13 @@ pub fn play(samples: Stream) -> Result<(), Box<dyn Error>> {
     {
         let mut pp = PLAYER.lock()?;
         if pp.is_none() {
-            let device = cpal::default_output_device()
+            let host = cpal::default_host();
+            let device = host.default_output_device()
                 .ok_or_else(|| Box::new(io::Error::from(
                     ErrorKind::ConnectionRefused)))?;
             let event_loop = cpal::EventLoop::new();
             let target_rate = cpal::SampleRate(SAMPLE_RATE as u32);
-            let format = cpal::Format {
+            let format = cpal::StreamConfig {
                 channels: 1,
                 sample_rate: target_rate,
                 data_type: cpal::SampleFormat::I16,
@@ -71,7 +75,7 @@ pub fn play(samples: Stream) -> Result<(), Box<dyn Error>> {
                 if let SDO { buffer: UTOB(mut out) } = data {
                     let out = &mut *out;
                     let nout = out.len();
-                    println!("run {}", nout);
+                    // println!("run {}", nout);
                     let mut samples = samples.lock().unwrap();
                     for i in 0..nout {
                         match samples.next() {
