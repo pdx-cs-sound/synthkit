356f1e0f32318cedc9a2ff3328cdd1267f36613c
Bart Massey <bart.massey@gmail.com>
2020-03-16 22:02:42 -0700

    fixed to compile with latest cpal; not tested yet

--- src/play.rs
+++ src/play.rs
@@ -9,7 +9,6 @@ use std::error::Error;
 use std::io::{self, ErrorKind};
 use std::sync::{Arc, Mutex};
 
-use cpal::*;
 use cpal::traits::*;
 
 use crate::*;
@@ -31,37 +30,29 @@ struct Player {
 pub fn play(samples: Stream) -> Result<(), Box<dyn Error>> {
 
     // Create and initialize cpal state.
-    {
-        let mut pp = PLAYER.lock()?;
-        if pp.is_none() {
-            let host = cpal::default_host();
-            let device = host.default_output_device()
-                .ok_or_else(|| Box::new(io::Error::from(
-                    ErrorKind::ConnectionRefused)))?;
-            let event_loop = cpal::EventLoop::new();
-            let target_rate = cpal::SampleRate(SAMPLE_RATE as u32);
-            let format = cpal::StreamConfig {
-                channels: 1,
-                sample_rate: target_rate,
-                data_type: cpal::SampleFormat::I16,
-            };
-            let player = Player {
-                device,
-                format,
-                event_loop,
-            };
-            *pp = Some(player);
-        }
+    let mut player = PLAYER.lock()?;
+    if player.is_none() {
+        let host = cpal::default_host();
+        let event_loop = host.event_loop();
+        let device = host.default_output_device()
+            .ok_or_else(|| Box::new(io::Error::from(
+                ErrorKind::ConnectionRefused)))?;
+        let target_rate = cpal::SampleRate(SAMPLE_RATE as u32);
+        let format = cpal::Format {
+            channels: 1,
+            sample_rate: target_rate,
+            data_type: cpal::SampleFormat::I16,
+        };
+        *player = Some(Player { event_loop, device, format });
     }
-    let player = PLAYER.lock()?;
     let player = match &*player {
         Some(p) => p,
         None => panic!("internal error: no player"),
     };
 
-    let stream = (&player.event_loop)
+    let stream_id = (&player.event_loop)
         .build_output_stream(&player.device, &player.format)?;
-    (&player.event_loop).play_stream(stream);
+    (&player.event_loop).play_stream(stream_id)?;
 
     let samples = &mut std::sync::Arc::new(std::sync::Mutex::new(samples));
     let (send, recv) = std::sync::mpsc::channel();
@@ -69,10 +60,10 @@ pub fn play(samples: Stream) -> Result<(), Box<dyn Error>> {
         let samples = samples.clone();
         let send = send.clone();
         scope.spawn(move || {
-            (&player.event_loop).run(move |stream, data| {
+            (&player.event_loop).run(move |stream_id, data| {
                 use cpal::UnknownTypeOutputBuffer::I16 as UTOB;
                 use cpal::StreamData::Output as SDO;
-                if let SDO { buffer: UTOB(mut out) } = data {
+                if let SDO { buffer: UTOB(mut out) } = data.unwrap() {
                     let out = &mut *out;
                     let nout = out.len();
                     // println!("run {}", nout);
@@ -87,7 +78,7 @@ pub fn play(samples: Stream) -> Result<(), Box<dyn Error>> {
                                 for j in i..nout {
                                     out[j] = 0;
                                 }
-                                (&player.event_loop).destroy_stream(stream);
+                                (&player.event_loop).destroy_stream(stream_id);
                                 send.send(()).unwrap();
                                 break;
                             },
