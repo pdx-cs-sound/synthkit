21ad02736ef3b41aafbf8cf0b2d4c8014c75f313
Bart Massey <bart.massey@gmail.com>
2020-04-17 13:40:31 -0700

    modified samplr slightly

--- examples/samplr.rs
+++ examples/samplr.rs
@@ -8,10 +8,12 @@ use synthkit::*;
 
 fn main() {
     // Get a signal from a WAV file.
-    let signal = get_sample("loop.wav").unwrap();
+    let wav = std::env::args().nth(1).unwrap();
+    let sound = get_sample(&wav).unwrap();
+    let sloop = Loop::new(&sound);
     // Play signal on audio output.
-    let samples = Box::new(signal.into_iter());
+    let samples = Box::new(sloop.into_iter());
     play(samples).unwrap();
     // Read and decode MIDI keys.
-    read_keys("Mobile Keys 49").unwrap();
+    let _keystream = read_keys("Mobile Keys 49").unwrap();
 }
